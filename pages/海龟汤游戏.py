f99476b4e2164c04b9f54d846b78e8a546b18d65
ReeceDoo <88834714+ReeceDoo@users.noreply.github.com>
2024-07-17 17:49:14 +0800

    Update 海龟汤游戏.py

--- pages/海龟汤游戏.py
+++ pages/海龟汤游戏.py
@@ -8,6 +8,8 @@ import streamlit as st
 from haiguitang_gamehost.prompt_template import haiguitang_system_template
 from haiguitang_gamehost.haiguitang import haiguitang_txt
 
+OPENAI_API_KEY = '''sk-RnkevZWgbGsrU6OoFa5730B144904556991dF0359b27B0F8'''
+
 st.title("海龟汤游戏主持")
 
 # 是否使用免费API
@@ -30,7 +32,8 @@ if st.session_state.free_api:
     # 提示
     st.warning("🔑 正在使用免费API")
     # openai_api_key = os.getenv("OPENAI_API_KEY")
-    openai_api_key = secrets.OPENAI_API_KEY
+    # openai_api_key = secrets.OPENAI_API_KEY
+    openai_api_key = OPENAI_API_KEY
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "ai", 
